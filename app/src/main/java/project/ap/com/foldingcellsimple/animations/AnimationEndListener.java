98432dbf36c095eb18d0a34ea46153a9bab06b6f
aditya <adityaprakash159@gmail.com>
2018-10-16 20:14:05 +0530

    Initial commit

---
+++ app/src/main/java/project/ap/com/foldingcellsimple/animations/AnimationEndListener.java
@@ -0,0 +1,20 @@
+package project.ap.com.foldingcellsimple.animations;
+
+import android.view.animation.Animation;
+
+/**
+ * Just sugar for code clean
+ */
+public abstract class AnimationEndListener implements Animation.AnimationListener {
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // do nothing
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // do nothing
+    }
+
+}
